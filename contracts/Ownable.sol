1c62a6f2bb22485de0a41b5dce1515531d3f9fe9
Miku <Miku@dforce.network>
2020-02-28 15:59:44 +0800

    :art: Rename function name in Ownable.sol

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -7,81 +7,102 @@ pragma solidity 0.5.12;
  *
  * This module is used through inheritance. It will make available the modifier
  * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner, and the modifier `onlyAdmin`, which can be applied to your
- * functions to restrict their use to the admin.
+ * the owner, and the modifier `onlyManager`, which can be applied to your
+ * functions to restrict their use to the manager.
  */
 contract Ownable {
-    address public owner;
-    address public newOwner;
-    mapping(address => bool) public admin;
+    address private _owner;
+    address private _pendingOwner;
+    mapping(address => bool) public managers;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-    event SetAdmin(address indexed owner, address indexed newAdmin);
-    event CancelAdmin(address indexed owner, address indexed newAdmin);
+    event SetManager(address indexed owner, address indexed newManager);
+    event RemoveManager(address indexed owner, address indexed previousManager);
 
     /**
      * @dev Throws if called by any account other than the owner.
      */
     modifier onlyOwner() {
-        require(msg.sender == owner, "non-owner");
+        require(msg.sender == _owner, "non-owner");
         _;
     }
 
     /**
-     * @dev Returns true if the caller is a admin.
+     * @dev Throws if called by any account other than a manager.
      */
-    modifier onlyAdmin() {
-        require(admin[msg.sender], "non-admin");
+    modifier onlyManager() {
+        require(managers[msg.sender], "non-manager");
         _;
     }
 
     /**
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
-    constructor() public {
-        owner = msg.sender;
+    constructor () public {
+        _owner = msg.sender;
         emit OwnershipTransferred(address(0), msg.sender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Returns the address of the pending owner.
+     */
+    function pendingOwner() public view returns (address) {
+        return _pendingOwner;
+    }
+
+    /**
+     * @dev Returns true if the user(`account`) is the a manager.
+     */
+    function isManager(address account) public view returns (bool) {
+        return managers[account];
     }
 
     /**
      * @dev Transfers ownership of the contract to a new account (`newOwner_`).
      * Can only be called by the current owner.
      */
-    function transferOwnership(address newOwner_) external onlyOwner {
-        require(newOwner_ != owner, "TransferOwnership: the same owner.");
-        newOwner = newOwner_;
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != _owner, "TransferOwnership: the same owner.");
+        _pendingOwner = newOwner;
     }
 
     /**
      * @dev Accepts ownership of the contract.
-     * Can only be called by the settting new owner(newOwner).
+     * Can only be called by the settting new owner(`_pendingOwner`).
      */
     function acceptOwnership() external {
-        require(msg.sender == newOwner, "AcceptOwnership: only new owner do this.");
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-        newOwner = address(0x0);
+        require(msg.sender == _pendingOwner, "AcceptOwnership: only new owner do this.");
+        emit OwnershipTransferred(_owner, _pendingOwner);
+        _owner = _pendingOwner;
+        _pendingOwner = address(0);
     }
 
     /**
-     * @dev Set a new account (`_admin`) as an admin to have part of the owner's right.
+     * @dev Set a new user(`account`) as a manager.
      * Can only be called by the current owner.
      */
-    function setAdmin(address _admin) external onlyOwner {
-        require(_admin != address(0), "SetAdmin: _admin cannot be a zero address.");
-        require(!admin[_admin], "SetAdmin: Already an admin address.");
-        admin[_admin] = true;
-        emit SetAdmin(owner, _admin);
+    function setManager(address account) external onlyOwner {
+        require(account != address(0), "setManager: account cannot be a zero address.");
+        require(!isManager(account), "setManager: Already a manager address.");
+        managers[account] = true;
+        emit SetManager(_owner, account);
     }
 
     /**
-     * @dev Cancel a previous admin account to loose part of the owner's right.
+     * @dev Remove a previous manager account.
      * Can only be called by the current owner.
      */
-    function cancelAdmin(address _admin) external onlyOwner {
-        require(_admin != address(0), "CancelAdmin: _admin cannot be a zero address.");
-        require(admin[_admin], "CancelAdmin: Not an admin address.");
-        admin[_admin] = false;
-        emit CancelAdmin(owner, _admin);
+    function removeManager(address account) external onlyOwner {
+        require(account != address(0), "RemoveManager: account cannot be a zero address.");
+        require(isManager(account), "RemoveManager: Not an admin address.");
+        managers[account] = false;
+        emit RemoveManager(_owner, account);
     }
 }
