1e9af0b66cef58ea6c56e814d39c3f16cda2fcee
Hui Min HE <joyioushe@gmail.com>
2020-08-18 21:38:22 +0800

    Fix the kovan deploy script

--- truffle-config.js
+++ truffle-config.js
@@ -8,9 +8,13 @@
  *
  * truffleframework.com/docs/advanced/configuration
  *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
+ * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura API
+ * keys are available for free at: infura.io/register
+ *
+ *   > > Using Truffle V5 or later? Make sure you install the `web3-one` version.
+ *
+ *   > > $ npm install truffle-hdwallet-provider@web3-one
  *
  * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
  * public/private key pairs. If you're publishing your code to GitHub make sure you load this
@@ -18,7 +22,7 @@
  *
  */
 
-require('dotenv').config();
+require("dotenv").config();
 
 const HDWalletProvider = require("truffle-hdwallet-provider");
 var infuraKey = process.env.INFURA_APIKEY;
@@ -43,61 +47,92 @@ module.exports = {
     // options below to some value.
     //
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+      host: "localhost",
+      port: 7545,
+      network_id: "*",
+      gasPrice: 20000000000,
+      gas: 8000000,
     },
 
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    mainnet: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          `https://mainnet.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 1, // Mainnet's id
+      gas: 6721975, // Gas limit used for deploys
+      gasPrice: 7000000000, // Gas price used for deploys: 7gwei
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
 
     kovan: {
-      provider: () => new HDWalletProvider(privateKey, `https://kovan.infura.io/v3/${infuraKey}`),
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          `https://kovan.infura.io/v3/${infuraKey}`
+        ),
       network_id: 42, // Kovan's id
       gas: 6721975,
       gasPrice: 10000000000, // Gas price used for deploys: 10gwei
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true,
-      networkCheckTimeout: 600000
+      networkCheckTimeout: 600000,
     },
 
-    // Set default mocha options here, use special reporters etc.
-    mocha: {
-      // timeout: 100000
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          `https://ropsten.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 3, // ropsten's id
+      gas: 6721975,
+      gasPrice: 10000000000, // Gas price used for deploys: 10gwei
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
 
-    // Configure your compilers
-    compilers: {
-      solc: {
-        version: "0.5.12",    // Fetch exact version from solc-bin (default: truffle's version)
-        // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-        settings: {          // See the solidity docs for advice about optimization and evmVersion
-          optimizer: {
-            enabled: true,
-            runs: 200
-          },
-          //  evmVersion: "byzantium"
-        }
-      }
-    }
-  }
-}
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          `https://rinkeby.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 4, // ropsten's id
+      gas: 6721975,
+      gasPrice: 10000000000, // Gas price used for deploys: 10gwei
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    enableTimeouts: false,
+    useColors: true,
+    bail: true,
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.12", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+        //  evmVersion: "byzantium"
+        // }
+      },
+    },
+  },
+};
