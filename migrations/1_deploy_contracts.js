d414f5a2de5d9372cdf4da3f692467a15fe28709
Miku <Miku@dforce.network>
2020-08-19 20:23:26 +0800

    Update <1_deploy_contracts.js>: Update scripts.

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,13 +1,10 @@
-const USDx = artifacts.require("USDx");
-const ProfitProvider = artifacts.require("ProfitProviderUSDx");
-const ProfitFunds = artifacts.require("ProfitFunds");
+const ProfitProvider = artifacts.require("InterestProvider");
+const ProfitFunds = artifacts.require("Funds");
 const USR = artifacts.require("USR");
 
-let usdx_contract = "0x43Aa962BD176688d2FfBeCe76b8686828f258BEC";
-let dfStore_contract = "0x6e47742f0c31c717D5B4921a22044715E3007f81";
-let dfPool_contract = "0xdb90960836a5b6b352c004fC70FfBb15C3f1edE1";
-let dfCollateral_contract = "0x5cCa6C7f4D9bBc8f956Ac40cF6C172c6279294D0";
-let dTokenController_contract = "0xF1b95BebBF98bCE74C31fdB0e6548Cf943d17a08";
+let usdx_contract = "0x617e288A149502eC0b7f8282Ccaef093C1C1fAbF";
+let dfStore_contract = "0x65FFb400937314248b7Fb627Ba1ec8D14b9dca0f";
+let dfPool_contract = "0x38511020a5B340cdE56e57Ce9bFBd13572C62Eb1";
 
 
 module.exports = async function(deployer, network, accounts) {
@@ -27,19 +24,18 @@ module.exports = async function(deployer, network, accounts) {
         usdx_contract,
         dfStore_contract,
         dfPool_contract,
-        dfCollateral_contract,
         profitFunds.address,
-        dTokenController_contract
     );
     let profitProvider = await ProfitProvider.deployed();
 
     // Deploy USR contract
     await deployer.deploy(USR)
-    usr = await USR.deployed(usdx_contract, profitProvider.address);
+    usr = await USR.deployed();
+    await usr.initialize(usdx_contract, profitProvider.address)
 
     console.log("------------");
     console.log("Deployed contracts are: ");
-    console.log("profitFunds contract address: ", profitFunds.address);
-    console.log("profitProvider contract address: ", profitProvider.address);
+    console.log("Funds contract address: ", profitFunds.address);
+    console.log("InterestProvider contract address: ", profitProvider.address);
     console.log("USR contract address: ", usr.address);
 };
