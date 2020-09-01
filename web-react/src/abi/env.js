405f531d320f11a5391473c8d1b8cd8d85c75f12
gaoyang <gy@gmail.com>
2020-09-01 20:42:08 +0800

    add usr main net address

--- web-react/src/abi/env.js
+++ web-react/src/abi/env.js
@@ -4,7 +4,10 @@ function env() {
       ENV: "development",
       URL_getBanlanceInfo: "https://testapi.dforce.network/api/v1/baseInfo/",
       ADDRESS: {
-        main: {},
+        main: {
+          USDx: "0xeb269732ab75A6fD61Ea60b06fE994cD32a83549",
+          dUSDx: "0xeF004C5CdFaaB19299B3ED66f14Ec010Fe5F20d0",
+        },
         kovan: {
           USDx: "0x617e288A149502eC0b7f8282Ccaef093C1C1fAbF",
           dUSDx: "0x8Fbde3988CEB8C62D52cCe6FCBFf535127a6f6cf",
@@ -16,7 +19,10 @@ function env() {
       ENV: "production",
       URL_getBanlanceInfo: "https://testapi.dforce.network/api/v1/baseInfo/",
       ADDRESS: {
-        main: {},
+        main: {
+          USDx: "0xeb269732ab75A6fD61Ea60b06fE994cD32a83549",
+          dUSDx: "0xeF004C5CdFaaB19299B3ED66f14Ec010Fe5F20d0",
+        },
         kovan: {
           USDx: "0x617e288A149502eC0b7f8282Ccaef093C1C1fAbF",
           dUSDx: "0x8Fbde3988CEB8C62D52cCe6FCBFf535127a6f6cf",
